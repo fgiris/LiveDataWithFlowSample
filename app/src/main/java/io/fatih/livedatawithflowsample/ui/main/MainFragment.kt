6ad72b3048fa6aee6d1e8ff2982480935ca8189f
Fatih <suleyman.fatih.giris@dnb.no>
2020-01-18 04:43:00 +0100

    - Initial commit

--- app/src/main/java/io/fatih/livedatawithflowsample/ui/main/MainFragment.kt
+++ app/src/main/java/io/fatih/livedatawithflowsample/ui/main/MainFragment.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Fatih Giris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.fatih.livedatawithflowsample.ui.main
 
 import android.os.Bundle
@@ -30,6 +46,7 @@ class MainFragment : DaggerFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        // Obtain ViewModel
         viewModel = ViewModelProviders.of(
             this,
             viewModelFactory
@@ -40,18 +57,22 @@ class MainFragment : DaggerFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         btnWeatherForecast.setOnClickListener {
+            // Weather forecast with one shot request
             findNavController().navigate(R.id.action_mainFragment_to_weatherForecastOneShotFragment)
         }
 
         btnWeatherForecastRealTimeLiveData.setOnClickListener {
+            // Weather forecast with using Live data & Flow (data stream)
             findNavController().navigate(R.id.action_mainFragment_to_weatherForecastDataStreamFragment)
         }
 
         btnWeatherForecastRealTimeFlow.setOnClickListener {
+            // Weather forecast with using only Flow (data stream)
             findNavController().navigate(R.id.action_mainFragment_to_weatherForecastDataStreamFlowFragment)
         }
 
         btnDarkMode.setOnClickListener {
+            // Enable dark mode
             viewModel.setTheme(Theme.DARK)
         }
     }
--- app/src/main/java/io/fatih/livedatawithflowsample/ui/weatherforecast/oneshot/WeatherForecastOneShotViewModel.kt
+++ app/src/main/java/io/fatih/livedatawithflowsample/ui/weatherforecast/oneshot/WeatherForecastOneShotViewModel.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Fatih Giris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.fatih.livedatawithflowsample.ui.weatherforecast.oneshot
 
 import androidx.lifecycle.LiveData
@@ -14,7 +30,7 @@ class WeatherForecastOneShotViewModel @Inject constructor(
 
     private val _weatherForecast = weatherForecastRepository
         .fetchWeatherForecast()
-        .asLiveData(viewModelScope.coroutineContext)
+        .asLiveData(viewModelScope.coroutineContext) // Use viewModel scope for auto cancellation
 
     val weatherForecast: LiveData<Result<Int>>
         get() = _weatherForecast
