629ca6bc18b1586f5ce5e393a78e3a2a82bc80f4
Fatih <suleyman.fatih.giris@dnb.no>
2020-01-28 23:50:49 +0100

    - Add flow operators when fetching real time weather forecast

--- app/src/main/java/io/fatih/livedatawithflowsample/ui/weatherforecast/datastreamflow/WeatherForecastDataStreamFlowViewModel.kt
+++ app/src/main/java/io/fatih/livedatawithflowsample/ui/weatherforecast/datastreamflow/WeatherForecastDataStreamFlowViewModel.kt
@@ -19,16 +19,72 @@ package io.fatih.livedatawithflowsample.ui.weatherforecast.datastreamflow
 import androidx.lifecycle.ViewModel
 import io.fatih.livedatawithflowsample.data.weatherforecast.WeatherForecastRepository
 import io.fatih.livedatawithflowsample.shared.Result
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 class WeatherForecastDataStreamFlowViewModel @Inject constructor(
     weatherForecastRepository: WeatherForecastRepository
 ) : ViewModel() {
 
     private val _weatherForecast = weatherForecastRepository
         .fetchWeatherForecastRealTime()
+        .distinctUntilChanged()
+        .filter {
+            // There could be millions of data when filtering
+            // Do some filtering
+            delay(2000)
+
+            // Let's add an additional filtering to take only
+            // data which is less than 10
+            if (it is Result.Success) {
+                it.data < 10
+            } else true // Do nothing if result is loading or error
+        }
+        .buffer()
+        .map {
+            // Do some heavy mapping
+            delay(500)
+
+            // Let's add an additional mapping to convert
+            // celsius degree to Fahrenheit
+            if (it is Result.Success) {
+                val fahrenheitDegree = convertCelsiusToFahrenheit(it.data)
+                Result.Success(fahrenheitDegree)
+            } else it // Do nothing if result is loading or error
+        }
+        .transform {
+            // Let's send only even numbers
+            if (it is Result.Success && it.data % 2 == 0) {
+                val evenDegree = it.data
+                emit(Result.Success(evenDegree))
+            } else emit(it) // Do nothing if result is loading or error
+        }
+        .onEach {
+            // Do something with the modified data. For instance
+            // save the modified data to cache
+            println("$it has modified and reached until onEach operator")
+        }
+        .flowOn(Dispatchers.Default) // Changes the context of flow
+        .catch { throwable ->
+            // Catch exceptions in all down stream flow
+            // Any error occurs after this catch operator
+            // will not be caught here
+            println(throwable)
+        }
 
     val weatherForecast: Flow<Result<Int>>
         get() = _weatherForecast
+
+    /**
+     * This function converts given [celsius] to Fahrenheit.
+     *
+     * Fahrenheit degree = Celsius degree * 9 / 5 + 32
+     *
+     * @return Fahrenheit integer for [celsius]
+     */
+    private fun convertCelsiusToFahrenheit(celsius: Int) = celsius * 9 / 5 + 32
 }
